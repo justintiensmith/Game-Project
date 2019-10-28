9897f0470f145792f4281220985d1d3b21d75f41
justintiensmith <justintiensmith@gmail.com>
2019-10-28 12:15:53 -0700

    Update sketch.js
    I made triangles and squares shoot across the screen with the balls

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -2,6 +2,8 @@
 //create an empty array called balls
 
 let balls = [];
+let rectangles = [];
+let triangles = [];
 
 function setup() {
   createCanvas(800, 400);
@@ -9,19 +11,33 @@ function setup() {
 }
 
 function draw(){
-	background(128,128,252);
+  background(252,128,128);
 
-//	draw all the balls in that array
+for (let i = 0; i < rectangles.length; i++) {
+  rectangles[i].drawRect();
+}
+
 	for (let i = 0; i < balls.length; i++) {
-	    balls[i].drawBall();
-      balls[i].moveBall();
-	  }
+	 balls[i].drawBall();
+   balls[i].moveBall();
+}
+
+	for (let i = 0; i < triangles.length; i++) {
+   triangles[i].drawTriangle();
+   triangles[i].moveTriangle();
+  }
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(100, 100);
+  let  b = new Ball(200, 200);
+  let  r = new Rect (100, 100);
+  let  t = new Triangle (250,100);
   balls.push(b);
   console.log(balls);
+  rectangles.push(r);
+  console.log(rectangles);
+  triangles.push(t);
+  console.log(triangles);
 }
 
 //ball class from which to create new balls with similar properties.
@@ -31,17 +47,48 @@ class Ball {
 		    this.x = x;
     		this.y = y;
 	}
-
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill("red");
-		    ellipse(this.x,this.y,10,10);
+  	drawBall(){  // draw a ball on the screen at x,y
+      		stroke(0);
+      		fill("red");
+  		    ellipse(this.x,this.y,10,10);
+  }
+  	moveBall(){ //update the location of the ball, so it moves across the screen
+  		this.x = this.x+2;
+  		this.y = this.y+.5;
 	}
+}
 
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
-	}
+class Rect {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
 
+  drawRect(){
+    stroke(0);
+    fill("blue")
+    rect(this.x,this.y,10,10)
+}
+  moveRect(){
+    this.x = this.x+1;
+    this.y = this.y+1
+  }
+}
 
+class Triangle {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  drawTriangle(){
+    stroke(0);
+    fill("green")
+    triangle(this.x,this.y, this.x+10,this.y-20,this.x+20,this.y)
+}
+  moveTriangle(){
+    this.x = this.x+2.5;
+    this.y = this.y+1
+
+  }
 }
